bf9eb48e49338daaf26b9fffa2f2987ff58e17a5
Andrew <chan.andrew1995@gmail.com>
2026-05-15 20:44:17 -0400

    Added vercel insights

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import 'leaflet/dist/leaflet.css';
 import { ThemeProvider } from '@mui/material';
 import Maps from './components/map/Map';
 import { theme } from './theme';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
 
   return (
     <ThemeProvider theme={theme}>
       <Maps />
+      <SpeedInsights />
     </ThemeProvider>
   )
 }
